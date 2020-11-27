cfe9c7dbe51585b4a2b5b99541f0962c57312587
nameruse2 <nameruse@protonmail.com>
2020-11-27 23:57:00 +0000

    change

--- reddit.py
+++ reddit.py
@@ -10,11 +10,17 @@ import datetime
 now = datetime.datetime.now()
 filename = now.strftime("%Y%m%d%f")
 
-r=requests.get("https://www.reddit.com/r/aww/comments/jzldmu/that_is_a_majestical_beast.json")
+url = 'https://www.reddit.com/r/nextfuckinglevel/comments/k1yr4p/strike_a_match_without_hands.json'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+print("Finding URL")
+r = requests.get(url, headers=headers)
+
 
 data=(r.json())
 video_url = requests.get(data[0]['data']['children'][0]['data']['secure_media']['reddit_video']['fallback_url'])
 
+print("Downloading Video")
 with open(f"{filename}.mp4", "wb") as f:
     for chunk in video_url.iter_content(100000):
         f.write(chunk)
